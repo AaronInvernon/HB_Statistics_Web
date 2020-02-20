0e60e3d1ae09e9428d83956102a920ac0b60c097
root <root@LAPTOP-TCKDMSCV.localdomain>
2020-02-20 17:57:24 +0100

    Login and register, created AuthContext and App

---
+++ src/components/misc/Login.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import HBService from '../../services/HBService';
+import { Link, Redirect } from 'react-router-dom';
+import { WithAuthConsumer } from '../../contexts/AuthContext';
+
+class Login extends React.Component {
+    state ={
+        data: {
+            email: '',
+            password: ''
+        }, 
+        error: false, 
+        loading: false
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target
+
+        this.setState({
+            data: {
+                ...this.state.data, 
+                [name]: value
+            }
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({ loading: true, error: false}, () => {
+            HBService.login({ ...this.state.data })
+            .then(
+                (user) => {
+                    this.props.setUser(user)
+                },
+                () => {
+                    this.setState({ error: true, loading: false })
+                }
+            )
+        })
+    } 
+
+    render() {
+        const errorClassName = this.state.error ? 'is-invalid' : ''
+        
+        if (this.props.currentUser) {
+            return <Redirect to="/"/>
+        }
+
+        return (
+            <div className="Login">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="email">Email: </label>
+                        <input 
+                            value={this.state.data.email}
+                            onchange={this.handleChange}
+                            autoComplete="off"
+                            name="email"
+                            typw="email"
+                            className={`form-control ${errorClassName}`}
+                            id="email"
+                            placeholder="Enter email"
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password">Password: </label>
+                        <input 
+                            value={this.state.data.password}
+                            onchange={this.handleChange}
+                            autoComplete="off"
+                            name="password"
+                            typw="password"
+                            className={`form-control ${errorClassName}`}
+                            id="password"
+                            placeholder="Enter password"
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn btn-primary mb-3"
+                        disabled={this.state.loading}
+                    >
+                        Login
+                    </button>
+
+                    <Link to="/signup"> Register</Link>
+                </form>
+            </div>
+        )
+    }   
+}
+
+export default WithAuthConsumer(Login);
---
+++ src/components/misc/Register.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import HBService from '../../services/HBService';
+import { Link, Redirect } from 'react-router-dom';
+import { WithAuthConsumer } from '../../contexts/AuthContext';
+
+class Register extends React.Component {
+    state = {
+        data: {
+            name: '',
+            email: '',
+            password: ''
+        },
+        error: false,
+        loading: false,
+        success: false
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target
+
+        this.setState({
+            data: {
+                ...this.state.data,
+                [name]: value
+            }
+        })
+    }
+
+    handleSubmit = (event) => {
+        const { data } = this.state
+
+        const formData = new FormData()
+        formData.append('name', data.name)
+        formData.append('email', data.email)
+        formData.append('password', data.password)
+
+        this.setState({ loading: true, error: false }, () => {
+            HBService.register(formData)
+                .then(() => {
+                    this.setState({ success: true })
+                })
+                .catch(() => {
+                    this.setState({ error: true, loading: false })
+                })
+        })
+    }
+
+    render() {
+        const errorClassName = this.state.error ? 'is-invalid' : ''
+
+        if (this.state.success) {
+            return <Redirect to="/login" />
+        }
+
+
+        return (
+            <div className="Register">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name: </label>
+                        <input
+                            value={this.state.data.name}
+                            onChange={this.handleChange}
+                            autoComplete="off"
+                            name="name"
+                            type="text"
+                            className={`form-control ${errorClassName}`}
+                            id="name"
+                            placeholder="Enter name"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+
+                        <input
+                            value={this.state.data.email}
+                            onChange={this.handleChange}
+                            autoComplete="off"
+                            name="email"
+                            type="email"
+                            className={`form-control ${errorClassName}`}
+                            id="email"
+                            placeholder="Enter email"
+                        />
+                    </div>
+
+                    <div className="form-group mb-5">
+                        <label htmlFor="password">Password</label>
+
+                        <input
+                            value={this.state.data.password}
+                            onChange={this.handleChange}
+                            name="password"
+                            type="password"
+                            className={`form-control ${errorClassName}`}
+                            id="password"
+                            placeholder="Password"
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn btn-primary mb-3"
+                        disabled={this.state.loading}
+                    >
+                        Sign up
+                    </button>
+                    <Link to="/login"> Log in</Link>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default WithAuthConsumer(Register)
